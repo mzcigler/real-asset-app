c38640f46e96b85c7b59d0fccd35a66b60c74df2
mzcigler <mzcigler14@gmail.com>
2026-03-26 17:57:55 +0100

    create env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://qmqgremvjkooevqpomuj.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_0W27A3dy1sa7zsSG8vZjZw_lTm26tFA';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
